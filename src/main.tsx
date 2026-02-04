2810087648ac679a4d6be768e4a1417103fcb02c
Jerome Austin <jeromeaustin@macbook-air-de-jerome.home>
2026-02-04 15:35:00 +0100

    Remplacement react-helmet-async par react-helmet

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Helmet } from "react-helmet";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HelmetProvider>
+    <Helmet>
       <App />
-    </HelmetProvider>
+    </Helmet>
   </StrictMode>,
-)
+);
