a7a3308b5c6dab9141f54bab568021ef2d8d2827
Jerome Austin <jeromeaustin@macbook-air-de-jerome.home>
2026-02-07 13:21:56 +0100

    feat: ajout ScrollToTop pour remonter en haut de page à chaque navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { LanguageProvider } from '@/contexts/LanguageContext';
-import { NavbarV7 as Navbar } from '@/components/custom/NavbarV7';
-import { FooterV7 as Footer } from '@/components/custom/FooterV7';
-import { HomePage } from '@/pages/HomePage';
-import { ColivingPageV4 as ColivingPage } from '@/pages/ColivingPageV4';
-import { ServicesPageV4 as ServicesPage } from '@/pages/ServicesPageV4';
-import { HousesPageV4 as HousesPage } from '@/pages/HousesPageV4';
-import { RatesPageV4 as RatesPage } from '@/pages/RatesPageV4';
-import { FAQPageV4 as FAQPage } from '@/pages/FAQPageV4';
-import { JoinPageV4 as JoinPage } from '@/pages/JoinPageV4';
-import { HouseDetailPage } from '@/pages/HouseDetailPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LanguageProvider } from "@/contexts/LanguageContext";
+import { NavbarV7 as Navbar } from "@/components/custom/NavbarV7";
+import { FooterV7 as Footer } from "@/components/custom/FooterV7";
+import { HomePage } from "@/pages/HomePage";
+import { ColivingPageV4 as ColivingPage } from "@/pages/ColivingPageV4";
+import { ServicesPageV4 as ServicesPage } from "@/pages/ServicesPageV4";
+import { HousesPageV4 as HousesPage } from "@/pages/HousesPageV4";
+import { RatesPageV4 as RatesPage } from "@/pages/RatesPageV4";
+import { FAQPageV4 as FAQPage } from "@/pages/FAQPageV4";
+import { JoinPageV4 as JoinPage } from "@/pages/JoinPageV4";
+import { HouseDetailPage } from "@/pages/HouseDetailPage";
+import { ScrollToTop } from "@/components/ScrollToTop"; // ← AJOUTEZ CETTE LIGNE
 
 function App() {
   return (
     <LanguageProvider>
       <BrowserRouter>
+        <ScrollToTop /> {/* ← AJOUTEZ CETTE LIGNE ICI */}
         <div className="min-h-screen bg-background">
           <Navbar />
           <Routes>
